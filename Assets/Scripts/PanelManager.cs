ff9e90390bed389f933414b589e442a77ffaa5d0
PA <pierre.alexis.odye@gmail.com>
2020-02-06 14:15:23 +0100

    Scene principal, volet d'info lorem ipsum, manque la transition slide

---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    RectTransform rt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        
+    }
+
+}
