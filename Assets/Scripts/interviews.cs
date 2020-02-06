9073fbe78eaa3797d4054e325bc2ff0464074f40
fostine18 <faustine_c@orange.fr>
2020-02-06 10:01:40 +0100

    Dépot initial branche interview

---
+++ Assets/Scripts/interviews.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interviews : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LanceVideo(id));
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RetourAccueil()
+    {
+    
+    }
+
+    IEnumerable LanceVideo(int id)
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while(videoPlayer.isPrepared)
+        {
+      
+ 
+        }
+    }
+}
