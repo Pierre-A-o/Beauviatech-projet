fda1bc5e3f49767e26d889c932ec324dd221f48a
Haeternal <hugo4m@hotmail.fr>
2020-02-17 10:35:23 +0100

    Accueil buttons functionals

---
+++ Assets/Scripts/AfficheSceneAppareil.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AfficheSceneAppareil : MonoBehaviour
+{
+
+    public Object SceneACharger;
+   
+    public void afficherScene(Object SceneACharger)
+    {
+        SceneManager.LoadScene(SceneACharger.name);
+    }
+
+}
--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AccueilManager : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
