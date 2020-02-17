02df0e7dfdb57efb14471e50ddd9fc09f22d1202
Haeternal <hugo4m@hotmail.fr>
2020-02-17 14:34:56 +0100

    Merge SceneAccueil

---
+++ Assets/Scripts/GestionMode.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestionMode : MonoBehaviour
+{
+    [SerializeField]
+    private bool Active3D;
+
+    public Button Boutton3D;
+    public Button BouttonVR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        set3D();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void set3D()
+    {
+        Active3D = true;
+        ColorBlock colorDisable = BouttonVR.colors;
+        colorDisable.normalColor = Color.black;
+        ColorBlock colorSelect = Boutton3D.colors;
+        colorSelect.normalColor = Color.red;        
+    }
+
+    public void setVR()
+    {
+        Active3D = false;
+        ColorBlock colorDisable = Boutton3D.colors;
+        colorDisable.normalColor = Color.black;
+        ColorBlock colorSelect = BouttonVR.colors;
+        colorSelect.normalColor = Color.red;
+    }
+}
