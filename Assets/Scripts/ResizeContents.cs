f466baeb203ff88c02229765d38c211133095163
Lambert Maxime <lambertmaxime.35000@gmail.com>
2020-02-20 13:48:47 +0100

    infoPanel fonctionne

---
+++ Assets/Scripts/ResizeContents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResizeContents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<GridLayoutGroup>().constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        GetComponent<GridLayoutGroup>().constraintCount = 1;
+        if (gameObject.transform.childCount > 0)
+        {
+            GetComponent<GridLayoutGroup>().cellSize = new Vector2(500f, 335 / gameObject.transform.childCount);
+        } else
+        {
+            GetComponent<GridLayoutGroup>().cellSize = new Vector2(500f, 335);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ResizeOnglets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResizeOnglets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<GridLayoutGroup>().constraint = GridLayoutGroup.Constraint.FixedRowCount;
+        GetComponent<GridLayoutGroup>().constraintCount = 1;
+        if (gameObject.transform.childCount > 0)
+        {
+            GetComponent<GridLayoutGroup>().cellSize = new Vector2(614 / gameObject.transform.childCount, GetComponent<GridLayoutGroup>().cellSize.y);
+        } else
+        {
+            GetComponent<GridLayoutGroup>().cellSize = new Vector2(614, GetComponent<GridLayoutGroup>().cellSize.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
