d577c95967b615f067584ef2588d4085b9008085
Lambert Maxime <lambertmaxime.35000@gmail.com>
2020-02-21 15:56:02 +0100

    version 1.0

---
+++ Assets/DetectHovering.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectHovering : MonoBehaviour
+{
+    public GameObject UIManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("je suis sur infopanel");
+        UIManager.GetComponent<ManipulationController>().pointingOnInfopanel = true;
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("je suis pas sur infopanel");
+        UIManager.GetComponent<ManipulationController>().pointingOnInfopanel = false;
+    }
+}
